5b861f6f6bef96fc23dc6fec7945adf272e35d23
somni <me@somni.one>
2022-11-06 17:56:01 +0900

    Use `sass` package instead of `node-sass`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   'use strict';
-  const sass = require('node-sass');
+  const sass = require('sass');
   require('load-grunt-tasks')(grunt, {
     pattern: ['grunt-*']
   });
